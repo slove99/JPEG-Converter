9a6ffcbcfac32d12b89411095058b8afffd53365
Samuel <slove99@github.com>
2020-08-09 17:12:18 +0100

    Added additional output

--- prog2Base.py
+++ prog2Base.py
@@ -1,16 +1,20 @@
 from wand.image import Image
 import os
 
-scanDir = "//192.168.1.82/Public\Samuel/EAC flac files"
-storeDir = "C:/Users/Samuel/Desktop/Converted"
+scanDir = "E:/Users/Samuel/Music/EAC flac files"
+storeDir = "E:/Users/Samuel/Pictures/ConvertedImages"
+
+unconvertedImageCount = 0
+totImageCount = 0
+percentConverted = 0
 
 for dirpath, dirs, files in os.walk(scanDir):
     for filename in files:
         fname = os.path.join(dirpath, filename)
-        if filename == "folder.jpg":
-            #print(fname)
+        if ".jpg" in filename:
             with Image(filename=fname) as i:
                 print(i.width)
+                totImageCount += 1
                 if(i.interlace_scheme == 'jpeg'):
                     i.interlace_scheme = 'no'
                     saveName = os.path.relpath(fname, start=scanDir)
@@ -18,8 +22,8 @@ for dirpath, dirs, files in os.walk(scanDir):
                     print(saveName)
                     saveLoc = os.path.join(storeDir, saveName)
                     i.save(filename=saveLoc)
-                #with i.convert('jpg') as converted:
-                    #converted.interlace_scheme = 'no'
-                 #   converted.save(filename='test.jpg')
-        #with open(fname) as myfile:
-        #    print(myfile.name)
+                    unconvertedImageCount += 1
+if totImageCount > 0:
+    percentConverted = (unconvertedImageCount / totImageCount) * 100
+print("Found %d interlaced images out of %d images \n %f%% images converted" % (unconvertedImageCount, totImageCount,
+                                                                               percentConverted))
